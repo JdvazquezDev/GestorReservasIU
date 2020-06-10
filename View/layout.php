6901c3bde449d38a18e5909f0c34505c1cc1e2d1
José Domínguez Vázquez <jdvazquezdev@gmail.com>
2020-06-10 19:38:15 +0200

    Add bootstrap content and layout.php

---
+++ View/layout.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'Functions/Authentication.php';
+if (!isset($_SESSION['idioma'])) {
+  $_SESSION['idioma'] = 'SPANISH';
+} else {
+}
+include 'Locale/Strings_' . $_SESSION['idioma'] . '.php';
+?>
+<html>
+
+  <head>
+    <title>
+      ReservasIU
+    </title>
+    <meta charset="UTF-8">
+
+    <!-- CSS STYLES -->
+    <link rel="stylesheet" type="text/css" href="View/css/layout.css" />
+    <link rel="stylesheet" href="./bootstrap-4.3.1-dist/css/bootstrap.min.css" />
+
+  </head>
+
+  <body class="d-flex flex-column">
+    <div id="page-content">
