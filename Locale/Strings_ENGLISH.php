95b485f25aeabfb48e741b8f11997c74f11d6e61
José Domínguez Vázquez <jdvazquezdev@gmail.com>
2020-06-11 18:42:54 +0200

    Update Strings_Languages files and add functionality to switch language

--- Locale/Strings_ENGLISH.php
+++ Locale/Strings_ENGLISH.php
@@ -8,4 +8,45 @@
 $strings =
 	array(
 
+		//----------------- USUARIO ----------------- 
+		'Login' => 'Login',
+		'Contraseña' => 'Password',
+		'Nombre' => 'Name',
+		'Apellidos' => 'Surname',
+		'Email' => 'Email',
+		'Rol' => 'Role',
+		'Usuario' => 'User',
+
+		//----------------- ESPACIO -----------------
+		'Espacios' => 'Spaces',
+
+		//----------------- RESERVA -----------------
+		'Reservas' => 'Bookings',
+
+
+		//----------------- IDIOMAS -----------------
+		'ENGLISH' => 'English',
+		'SPANISH' => 'Spanish',
+		'GALLAECIAN' => 'Gallaecian',
+		'Idioma' => 'Language',
+
+		//----------------- PALABRAS -----------------
+		'Inicio' => 'Home',
+		'Creado por:' => 'Created by:',
+		'aquí' => 'here',
+		'Si no tienes cuenta, crea una ' => 'If you dont have an account, create one ',
+		'Si ya tienes una cuenta, inicia sesión' => 'If you already have an account, log in',
+
+		//----------------- ACCIONES -----------------
+		'AÑADIR' => 'ADD',
+		'SHOWALL' => 'SHOWALL',
+		'EDITAR' => 'EDIT',
+		'BORRAR' => 'DELETE',
+		'BUSCAR' => 'SEARCH',
+		'SHOWCURRENT' => 'SHWOCURRENT',
+		'Volver' => 'Go Back',
+		'Iniciar sesión' => 'Log in',
+		'Crear cuenta' => 'Create an account'
+
+
 	);
